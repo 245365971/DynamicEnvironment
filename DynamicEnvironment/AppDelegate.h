c8bc352dce672656307c3cfb2feae8e35cc125eb
hui <245365971@qq.com>
2015-08-28 09:32:04 +0800

    项目搭建

---
+++ DynamicEnvironment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicEnvironment
+//
+//  Created by w gq on 15/8/28.
+//  Copyright (c) 2015年 zjsos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
